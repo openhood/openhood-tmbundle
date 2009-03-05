b0c59b25c1a01f16c85dcacee35558489950cd44
Joseph HALTER <joseph@openhood.com>
2009-03-05 10:37:53 +0100

    Update to use Rails frozen in project instead of as gem if available.

---
+++ Support/lib/dependencies.rb
@@ -0,0 +1,14 @@
+RAILS_ROOT = ENV["TM_PROJECT_DIRECTORY"]
+if File.directory?("#{RAILS_ROOT}/vendor/rails/")
+  # we have found Rails frozen in vendor, use it from there
+  $:.unshift "#{RAILS_ROOT}/vendor/rails/activesupport/lib/"
+  require "active_support"
+else
+  # Rails not found, try to load what we need as gem
+  begin
+    require "rubygems"
+    require "active_support"
+  rescue
+    TextMate.exit_show_tool_tip "Error: Rails not found either frozen or as gem."
+  end
+end
